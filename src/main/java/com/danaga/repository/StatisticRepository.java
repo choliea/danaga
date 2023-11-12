38ffd23936dab91d96111d6bc21d5c9c9dfc524e
yukang <choliea91@gmail.com>
2023-11-12 18:31:01 +0900

    convert statistic query to jpql

--- src/main/java/com/danaga/repository/StatisticRepository.java
+++ src/main/java/com/danaga/repository/StatisticRepository.java
@@ -12,37 +12,37 @@ import com.danaga.entity.Statistic;
 
 public interface StatisticRepository extends JpaRepository<Statistic, String>{
 	// N일 총 판매수량
-	@Query(value = "select nvl(count(create_date),0) from orders where to_char(create_date,'YYYYMMDD') = :findDate", nativeQuery = true)
+	@Query("select count(o) from Order o where FUNCTION('DATE_FORMAT', o.createDate, '%Y%m%d') = :findDate")
 	Long countTotSalesOn(@Param("findDate") String findDate);
-	
+
 	// N일 총 판매수익
-	@Query(value = "select nvl(sum(price),0) from orders where to_char(create_date,'YYYYMMDD') = :findDate", nativeQuery = true)
+	@Query("select coalesce(sum(o.price), 0) from Order o where FUNCTION('DATE_FORMAT', o.createDate, '%Y%m%d') = :findDate")
 	Long countTotRevenueOn(@Param("findDate") String findDate);
-	
+
 	// N일 가입한 신규회원 수
-	@Query(value = "select nvl(count(id),0) from member where to_char(join_date,'YYYYMMDD') = :findDate", nativeQuery = true)
+	@Query("select count(m) from Member m where FUNCTION('DATE_FORMAT', m.joinDate, '%Y%m%d') = :findDate")
 	Long countNewMembersOn(@Param("findDate") String findDate);
-	
+
 	// N일 작성된 게시글 수 완료
-	@Query(value = "select nvl(count(id),0) from board where to_char(create_time,'YYYYMMDD') = :findDate", nativeQuery = true)
+	@Query("select count(b) from Board b where FUNCTION('DATE_FORMAT', b.createTime, '%Y%m%d') = :findDate")
 	Long countNewBoardsOn(@Param("findDate") String findDate);
-	
-	//최근 7일간의 통계 기록
-	List<Statistic> findTop7ByOrderByIdDesc();
-	
+
+	// 최근 7일간의 통계 기록
+	List<Statistic> findTop7ByOrderByCreateDateDesc();
+
 	// YYYYMM월의 기록
-	List<Statistic> findByIdStartsWith(String id);
-	
+	List<Statistic> findByIdStartingWith(String id);
+
 	// M월 총 주문 건수
-	@Query(value = "select nvl(count(create_date),0) from orders where to_char(create_date,'YYYYMM') = :findMonth", nativeQuery = true)
+	@Query("select count(o) from Order o where FUNCTION('DATE_FORMAT', o.createDate, '%Y%m') = :findMonth")
 	Long countTotSalesThisMonth(@Param("findMonth") String findMonth);
-	
+
 	// M월 배송중+입금대기 건수
-	@Query(value = "select nvl(count(create_date),0) from orders where to_char(create_date,'YYYYMM') = :findMonth  and (statement = '배송중' or statement = '입금대기중')", nativeQuery = true)
+	@Query("select count(o) from Order o where FUNCTION('DATE_FORMAT', o.createDate, '%Y%m') = :findMonth and (o.statement = '배송중' or o.statement = '입금대기중')")
 	Long countToSalesThisMonth(@Param("findMonth") String findMonth);
-	
+
 	// 환불대기+환불완료+취소 건수
-	@Query(value = "select nvl(count(create_date),0) from orders where to_char(create_date,'YYYYMM') = :findMonth  and (statement = '환불대기중' or statement = '환불완료' or statement = '취소')", nativeQuery = true)
+	@Query("select count(o) from Order o where FUNCTION('DATE_FORMAT', o.createDate, '%Y%m') = :findMonth and (o.statement = '환불대기중' or o.statement = '환불완료' or o.statement = '취소')")
 	Long countFailSalesThisMonth(@Param("findMonth") String findMonth);
 
 }
